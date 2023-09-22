ab3617fbd0f103ec6344f4348afe8ac9481240e6
Somadrita Halder <78261391+Rai2020-ops@users.noreply.github.com>
2023-09-22 17:20:37 +0530

    Added solution - LeetHub

---
+++ Longest Common Prefix in an Array - GFG/longest-common-prefix-in-an-array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    
+    string longestCommonPrefix (string arr[], int N)
+    {
+        sort(arr,arr+N);
+        string first=arr[0];
+        string last=arr[N-1];
+        string prefix="";
+        int i=0;
+        while(i<first.length() && first[i]==last[i]){
+            prefix+=first[i];
+            i++;
+        }
+        if(prefix.length()!=0){
+            return prefix;
+        }
+        return "-1";
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        string arr[n];
+        for (int i = 0; i < n; ++i)
+            cin >> arr[i];
+        
+        Solution ob;
+        cout << ob.longestCommonPrefix (arr, n) << endl;
+    }
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
